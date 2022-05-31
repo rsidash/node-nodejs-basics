eb31630ac57848a4fec7845f448787a1754f1319
Roman Sidash <sidash.roman@mail.ru>
2022-06-01 01:12:39 +0600

    feat: implement create new file

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,3 +1,14 @@
+import path from 'path';
+import { writeFile } from 'fs';
+
 export const create = async () => {
-    // Write your code here 
+    const dir = 'files';
+    const fileName = 'fresh.txt';
+    const content = 'I am fresh and young';
+
+    const filePath = path.join(dir, fileName);
+
+    writeFile(filePath, content, { flag: 'wx' }, (err) => {
+        if (err) throw new Error('FS operation failed');
+    });
 };
