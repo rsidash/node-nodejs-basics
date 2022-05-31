7a9d0e37ba7a744583a213d6230c44e46e81b7f8
Roman Sidash <sidash.roman@mail.ru>
2022-06-01 02:11:29 +0600

    feat: implement copy files from folder to dest

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,25 @@
+import path from 'path';
+import { exists, mkdir, readdir, copyFile, constants } from 'fs';
+
 export const copy = async () => {
-    // Write your code here 
+    const srcDir = 'files';
+    const destDir = 'files_copy';
+
+    readdir(srcDir, (err, files) => {
+        if (err) throw new Error('Error occured during reading files from directory');
+
+        if (!exists(destDir, () => {})) {
+            mkdir(destDir, () => {});
+        }
+
+        files.forEach(file => {
+            let srcPath = path.join(srcDir, file);
+            let destPath = path.join(destDir, file);
+
+            copyFile(srcPath, destPath, constants.COPYFILE_EXCL, (err) => {
+                if (err) throw new Error('FS operation failed');
+            });
+        });
+
+      });
 };
