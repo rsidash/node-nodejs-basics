4c79cdc90821c8c5be406bf78791395b6624ac06
Roman Sidash <sidash.roman@mail.ru>
2022-06-01 16:20:30 +0600

    feat: implement read file

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,15 @@
+import path from 'path';
+import { readFile } from 'fs';
+
 export const read = async () => {
-    // Write your code here 
+    const dir = 'files';
+    const fileName = 'fileToRead.txt';
+
+    const filePath = path.join(dir, fileName);
+
+    readFile(filePath, 'utf8', (err, data) => {
+        if (err) throw new Error('FS operation failed');
+
+        console.log(data);
+      });
 };
