b76a24aef2ec8a4b5977b94d0975284b83d2532e
Roman Sidash <sidash.roman@mail.ru>
2022-06-01 15:56:17 +0600

    feat: implement rename file

--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,3 +1,19 @@
+import path from 'path';
+import { exists, rename as renameFile } from 'fs';
+
 export const rename = async () => {
-    // Write your code here 
+    const dir = 'files';
+    const oldFile = 'wrongFilename.txt';
+    const newFile = 'properFilename.md';
+
+    const oldPath = path.join(dir, oldFile);
+    const newPath = path.join(dir, newFile);
+
+    exists(newPath, (err) => { 
+        if (err) throw new Error('FS operation failed');
+
+        renameFile(oldPath, newPath, (err) => {
+            if (err) throw new Error('FS operation failed');
+        });
+    }) 
 };
