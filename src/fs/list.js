a8ec26aaffdcfa7c2abfb2f74fb6271e716fc6b3
Roman Sidash <sidash.roman@mail.ru>
2022-06-01 16:14:05 +0600

    feat: implement print filenames from directory

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,13 @@
+import { readdir } from 'fs';
+
 export const list = async () => {
-    // Write your code here 
+    const dir = 'files';
+
+    readdir(dir, (err, files) => {
+        if (err) throw new Error('FS operation failed');
+
+        files.forEach(file => {
+            console.log(file);
+        });
+    })
 };
