8b48e47b34397778f0e981a4d874914367e4e282
Roman Sidash <sidash.roman@mail.ru>
2022-06-01 16:02:03 +0600

    feat: implement delete file

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,15 @@
+import path from 'path';
+import { unlink } from 'fs';
+
 export const remove = async () => {
-    // Write your code here 
+    const dir = 'files';
+    const fileName = 'fileToRemove.txt';
+
+    const filePath = path.join(dir, fileName);
+
+    unlink(filePath, (err) => {
+        if (err) throw new Error('FS operation failed');
+    });
 };
+
+remove();
