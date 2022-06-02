82ac8199cde5633f84493ff0e7b7eaaab61e109e
Roman Sidash <sidash.roman@mail.ru>
2022-06-02 22:07:26 +0600

    feat: implement calculate hash for file

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,23 @@
+import path from 'path';
+import { createHash } from 'crypto';
+import { createReadStream } from 'fs';
+
 export const calculateHash = async () => {
-    // Write your code here 
+    const dir = 'files';
+    const fileName = 'fileToCalculateHashFor.txt';
+
+    const filePath = path.join(dir, fileName);
+
+    const hash = createHash('sha256');
+
+    const input = createReadStream(filePath);
+
+    input.on('readable', () => {
+    const data = input.read();
+    if (data)
+        hash.update(data);
+    else {
+            console.log(hash.digest('hex'));
+        }
+    });
 };
