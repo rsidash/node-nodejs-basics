fa9b87108be4d9cf2f014f052a201355056423b4
Roman Sidash <sidash.roman@mail.ru>
2022-06-04 23:32:00 +0600

    feat: implement transform data

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,23 @@
+import { Transform, pipeline } from 'stream';
+import { EOL } from 'os';
+
 export const transform = async () => {
-    // Write your code here 
+    const read = process.stdin;
+    const write = process.stdout;
+
+    const transform = new Transform({
+        transform(chunk) {
+          const reverseChunk = chunk.reverse();
+
+          this.push(reverseChunk + EOL);
+
+          process.exit();
+        }
+    });
+
+    pipeline(read, transform, write, err => {
+        if (err) throw new Error('Some error occured');
+    });
 };
+
+transform();
