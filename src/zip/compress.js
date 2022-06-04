2e75638fc62db07156ff39461c9dc4b4942eb6bc
Roman Sidash <sidash.roman@mail.ru>
2022-06-04 13:24:50 +0600

    feat: implement compress file to .gz

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,16 @@
+import path from 'path';
+import { createReadStream, createWriteStream } from 'fs';
+import { createGzip } from 'zlib';
+
 export const compress = async () => {
-    // Write your code here 
+    const dir = 'files'
+    const fileName = 'fileToCompress.txt';
+    const archiveName = 'archive.gz';
+
+    const filePath = path.join(dir, fileName);
+    const archivePath = path.join(dir, archiveName);
+
+    const readStream = createReadStream(filePath);
+    const writeStream = createWriteStream(archivePath);
+    readStream.pipe(createGzip()).pipe(writeStream);
 };
